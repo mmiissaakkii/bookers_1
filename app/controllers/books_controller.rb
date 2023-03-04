135031a84cd14b4798642c235e3d725fe6771631
mmiissaakkii <mmmnn1228@gmail.com>
2023-03-04 08:04:41 +0000

    books/index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,40 @@
 class BooksController < ApplicationController
+  
+  #投稿作成機能
+  def create
+    @book = Book.new(book_params)
+  end
+  
+  #投稿編集ページ
   def edit
+    @book =Book.find(params[:id])
   end
-
+  
+  #投稿編集機能
+  def update
+    @book =Book.find(params[:id])
+  end
+  
+  #投稿削除機能
+  def destroy
+    @book =Book.find(params[:id])
+  end
+  
+  #投稿一覧・作成ページ
   def index
+    @book_new = Book.new
+    @books = Book.all
   end
-
+  
+  #投稿詳細ページ
   def show
+    @book = Book.find(params[:id])
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).parmit(:title, :body)
+  end
+  
 end
